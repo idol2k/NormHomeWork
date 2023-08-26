2b9f5d9c6c199a3af782898104c792d948f8faf1
zxc.idol <ViperX_YT@mail.ru>
2023-08-26 15:21:44 +0300

    Validation

--- HW11/HomeWork11.2.py
+++ HW11/HomeWork11.2.py
@@ -1,7 +1,7 @@
 import re 
-
-input = str(input('Введите свой e-mail адрес:'))
-
-text = r"^(?!\.)(?!.*\.@)(?!.*\.\.)[\w.!#  %&'*+—/=?^_`{|}~]+@(?!\-)[\w{63}\-]+[\w.]+(?!\-)$"
-result = re.match(text, input) is not None
-print(result)
+input = str(input('Введите свой e-mail адрес:'))    
+def e_mail(self):
+    text = r"^(?!\.)(?!.*\.@)(?!.*\.\.)[\w.!#  %&'*+—/=?^_`{|}~]+@(?!\-)[\w{63}\-]+[\w.]+(?!\-)$"
+    result = re.match(text, input) is not None
+    print(result)
+e_mail(input)
